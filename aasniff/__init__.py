bcf2eb379f96578fe39c10ce54bfc1e5c9f133ea
Alexandre Leray <alexandre@stdin.fr>
2014-11-04 12:38:31 +0100

    Fix import error

--- aasniff/__init__.py
+++ aasniff/__init__.py
@@ -158,7 +158,7 @@ class AAApp(object):
 from sniffers.http import HttpSniffer
 from sniffers.feed import FeedSniffer
 from sniffers.html import HtmlSniffer
-from sniffers.html import TurtleSniffer
+from sniffers.turtle import TurtleSniffer
 from sniffers.image import ImageSniffer
 from sniffers.ogg import OggSniffer
 from sniffers.rdf import RDFSniffer
